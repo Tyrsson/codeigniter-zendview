a19dcbc5951d8ac990f143b7d09e61234c1f5cb8
Joey <jsmith@dirextion.com>
2013-07-25 14:11:25 -0500

    - Commiting changes in admin index.php

--- public_html/application/modules/admin/views/index.php
+++ public_html/application/modules/admin/views/index.php
@@ -1,8 +1,7 @@
 <?php
 $this->doctype('XHTML11');
 ?>
-<div class="testing">
+
     <?php
         echo $this->pod($this->podData, array('tag' => 'div', 'id' => 'menuPod', 'class' => 'menuPodClass'), array('ulClass' => 'classForUl'), true);
     ?>
-</div>
