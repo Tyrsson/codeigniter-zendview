48a1f5ecc14b6030872c21fb7dcc134a0057fd71
Joey <jsmith@dirextion.com>
2013-07-25 12:51:35 -0500

    - Adding Site view helpers for pods and pod menus - See: Site_View_Helper_Pod, Site_View_Helper_PodMenu

--- public_html/application/modules/admin/views/index.php
+++ public_html/application/modules/admin/views/index.php
@@ -4,6 +4,7 @@ $this->doctype('XHTML11');
 <div class="testing">
     <?php
         echo $this->testVar;
-        echo $this->myHelper('Some content for the pod');
+        //echo $this->myHelper('Some content for the pod');
+        echo $this->pod($this->podData, array('tag' => 'div', 'id' => 'menuPod', 'class' => 'menuPodClass'), array('ulClass' => 'classForUl'), true);
     ?>
 </div>
