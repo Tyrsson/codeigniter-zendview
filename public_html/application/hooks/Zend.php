7e06684781600d31e0519d30798e1986011366a7
Tyrsson <jsmith@webinertia.net>
2013-07-30 21:56:12 -0500

    Update Zend.php
    general cleanup

--- public_html/application/hooks/Zend.php
+++ public_html/application/hooks/Zend.php
@@ -6,10 +6,6 @@
  */
 class LoadZend {
 
-//     public function __construct() {
-
-
-//     }
     public function AutoLoad() {
         ini_set('include_path',
         ini_get('include_path') . PATH_SEPARATOR . APPPATH . 'libraries');
@@ -17,7 +13,6 @@ class LoadZend {
         require_once 'Zend/Loader/Autoloader.php';
         $autoloader = Zend_Loader_Autoloader::getInstance();
         $autoloader->registerNamespace('Site_');
-        //spl_autoload_register(array('Zend_Loader_Autoloader','autoload'));
         //log_message('debug', "Zend Class Initialized");
     }
 }
